0327cabc5f15758a74605739de264aacea65a534
danypype <danypype@gmail.com>
2014-12-30 18:05:11 -0500

    Added lib and test files. Updated package.json with dependencies

---
+++ test/test.js
@@ -0,0 +1,135 @@
+var CachedRequest = require("../")
+,   request = require("request")
+,   nock = require("nock")
+,   sh = require("execSync")
+,   Readable = require("stream").Readable
+,   util = require("util");
+
+util.inherits(MockedResponseStream, Readable);
+
+function MockedResponseStream (options, response) {
+  Readable.call(this, options);
+  this.response = response;
+}
+
+MockedResponseStream.prototype._read = function (size) {
+  this.push(this.response);
+  this.push(null);
+};
+
+describe("CachedRequest", function () {
+  var cacheDir = __dirname + "/cache";
+
+  function mock (method, times, response) {
+    method = method.toLowerCase();
+    times = times || 1;
+    nock("http://ping.com")
+      .filteringPath(/.+/, "/")
+      [method]("/")
+      .times(times)
+      .reply(200, response);
+  };
+
+  before(function () {
+    sh.exec("mkdir " +  cacheDir);
+    nock.disableNetConnect();
+  });
+
+  beforeEach(function () {
+    sh.exec("rm " +  cacheDir + "/*");
+    this.cachedRequest = CachedRequest(request);
+    this.cachedRequest.setCacheDirectory(cacheDir);
+  });
+
+  describe("caching", function () {
+    it("makes the request when the response isn't cached", function (done) {
+      mock("GET", 1, function () {
+        return new MockedResponseStream({}, "pong");
+      });
+      this.cachedRequest({uri: "http://ping.com/", ttl: 0}, function (error, response, body) {
+        if (error) return done(error);
+        expect(response.statusCode).to.equal(200);
+        expect(response.headers["x-from-cache"]).to.not.exist;
+        expect(body).to.equal("pong");
+        done();
+      });
+    });
+
+    it("responds from the cache", function (done) {
+      var self = this;
+      var responseBody = {"a": 1, "b": {"c": 2}};
+      var options = {
+        uri: "http://ping.com/",
+        method: "POST",
+        json: {
+          a: 1
+        },
+        ttl: 5000
+      };
+
+      mock(options.method, 1, function () {
+        return new MockedResponseStream({}, JSON.stringify(responseBody));
+      });
+
+      this.cachedRequest(options, function (error, response, body) {
+        if (error) return done(error);
+        expect(response.statusCode).to.equal(200);
+        expect(response.headers["x-from-cache"]).to.not.exist;
+        expect(body).to.deep.equal(responseBody);
+
+        self.cachedRequest(options, function (error, response, body) {
+          if (error) return done(error);
+          expect(response.statusCode).to.equal(200);
+          expect(response.headers["x-from-cache"]).to.equal(1);
+          expect(body).to.deep.equal(responseBody);
+          done();
+        });
+      });
+    });
+  });
+
+  describe("streaming", function () {
+    it("allows to use request as a stream", function (done) {
+      var self = this;
+      var responseBody = "";
+
+      for (var i = 0; i < 1000; i++) {
+        responseBody += "this is a long response body";
+      };
+
+      mock("GET", 1, function () {
+        return new MockedResponseStream({}, responseBody);
+      });
+
+      var options = {url: "http://ping.com/", ttl: 5000};
+      var body = "";
+
+      //Make fresh request
+      this.cachedRequest(options)
+      .on("data", function (data) {
+          body += data;
+      })
+      .on("end", function () {
+        expect(body).to.equal(responseBody);
+        body = "";
+        //Make cached request
+        self.cachedRequest(options)
+        .on("response", function (response) {
+          expect(response.statusCode).to.equal(200);
+          expect(response.headers["x-from-cache"]).to.equal(1);
+          response.on("data", function (data) {
+            body += data;
+          })
+          .on("end", function () {
+            expect(body).to.equal(responseBody);
+            done();
+          });
+        });
+      });
+    });
+  });
+
+  after(function () {
+    sh.exec("rm -rf " +  cacheDir);
+  });
+});
