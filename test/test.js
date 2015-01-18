048aac6088140a43ab5df8a2242affab2813be6d
Ben Smith <ben@10consulting.com>
2015-01-18 22:48:03 +0000

    Support for gzip compressed response
    Do not compress the cached response if it was already gzip compressed.
    
    Applicable when the `gzip: true` request option is used and the server
    has support.

--- test/test.js
+++ test/test.js
@@ -3,7 +3,8 @@ var CachedRequest = require("../")
 ,   nock = require("nock")
 ,   temp = require('temp').track()
 ,   Readable = require("stream").Readable
-,   util = require("util");
+,   util = require("util")
+,   zlib = require("zlib");
 
 util.inherits(MockedResponseStream, Readable);
 
@@ -20,14 +21,14 @@ MockedResponseStream.prototype._read = function (size) {
 describe("CachedRequest", function () {
   var cacheDir;
 
-  function mock (method, times, response) {
+  function mock (method, times, response, headers) {
     method = method.toLowerCase();
     times = times || 1;
     nock("http://ping.com")
       .filteringPath(/.+/, "/")
       [method]("/")
       .times(times)
-      .reply(200, response);
+      .reply(200, response, headers);
   };
 
   before(function () {
@@ -116,6 +117,49 @@ describe("CachedRequest", function () {
         .on("response", function (response) {
           expect(response.statusCode).to.equal(200);
           expect(response.headers["x-from-cache"]).to.equal(1);
+          response.on("data", function (data) {
+            body += data;
+          })
+          .on("end", function () {
+            expect(body).to.equal(responseBody);
+            done();
+          });
+        });
+      });
+    });
+
+    it("handles gzip response", function (done) {
+      var self = this;
+      var responseBody = "";
+
+      for (var i = 0; i < 1000; i++) {
+        responseBody += "this is a long response body";
+      };
+
+      //Return gzip compressed response with valid content encoding header
+      mock("GET", 1, function () {
+        return new MockedResponseStream({}, responseBody).pipe(zlib.createGzip());
+      }, 
+      {
+        "Content-Encoding": "gzip"
+      });
+
+      var options = {url: "http://ping.com/", ttl: 5000};
+      var body = "";
+
+      //Make fresh request
+      this.cachedRequest(options)
+      .on("data", function (data) {
+        //Ignore first reply
+      })
+      .on("end", function () {
+        body = "";
+        //Make cached request
+        self.cachedRequest(options)
+        .on("response", function (response) {
+          expect(response.statusCode).to.equal(200);
+          expect(response.headers["x-from-cache"]).to.equal(1);
+          expect(response.headers["content-encoding"]).to.equal("gzip");
           response.on("data", function (data) {
             body += data;
           })
